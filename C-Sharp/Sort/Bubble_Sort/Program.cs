9ab6da5c8864edc5369f8616bd7297bbf1a707d6
Ahmed <30933484+Ahmed-Manai@users.noreply.github.com>
2023-10-05 13:48:06 +0000

    feat(C-Sharp-Sort): Bubble and Selection Sort

---
+++ C-Sharp/Sort/Bubble_Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Time Complexity = O(n^2)
+Best Case = O(n) 
+Wost Case = O(n^2)
+*/
+
+
+// [5, 9, 3, 1, 2, 8, 4, 7, 6]
+//                       ^i
+//                          ^j
+
+int[] Bubble_Sort(int[] tab){
+    int temp;
+    bool swapped;
+    for (int i= 0; i<tab.Length -1 ; i++){ // Outer loop - O(n)
+        swapped = false;
+        for (int j=tab.Length-1; j>i; j--){ // Inner loop - O(n)
+            if (tab[j] < tab[j-1]){
+                temp =tab[j];
+                tab[j] = tab[j-1];
+                tab[j-1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped){
+            break;
+        }
+    }
+    return tab;
+}
+
+int[] in_tab = {5, 9, 3, 1, 2, 8, 4, 7, 6};
+
+int[] out_tab = Bubble_Sort(in_tab);
+
+Console.WriteLine("[ " + string.Join(", ", out_tab) + " ]");
---
+++ C-Sharp/Sort/Selection_Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Time Complexity = O(n^2)
+*/
+
+/* 
+[5, 9, 3, 1, 2, 8, 4, 7, 6]
+    ^i
+       ^j
+    ^min
+
+*/
+int[] Selection_Sort(int[] tab){
+
+    int temp;
+    for(int i=0; i<tab.Length-1; i++){ // Outer loop - O(n)
+        int min=i; 
+        for(int j=i+1; j<tab.Length; j++){ // Inner loop - O(n)
+            if (tab[min] > tab[j]){
+                min = j;
+            }
+        }
+        if(min != i)
+        {
+            temp = tab[i];
+            tab[i] = tab[min];
+            tab[min] = temp;
+        }
+    }
+
+    return tab;
+}
+
+
+int[] in_tab = {5, 9, 3, 1, 2, 8, 4, 7, 6};
+
+int[] out_tab = Selection_Sort(in_tab);
+
+Console.WriteLine("[ " + string.Join(", ", out_tab) + " ]");
