35dc22e499de3191212fcab273525fe3fb8dd45c
Ahmed <30933484+Ahmed-Manai@users.noreply.github.com>
2023-10-06 19:49:30 +0000

    feat(Insertion-Sort): Insertion Sort implementation

---
+++ C-Sharp/Sort/Insertion_Sort/Program.cs
@@ -0,0 +1,39 @@
+﻿
+/*
+Time Complexity = O(n^2)
+Best Case = O(n)
+Worst Case = O(n^2)
+*/
+
+
+/*
+[5, 9, 3, 1, 2, 8, 4, 7, 6]
+    ^i
+    ^j
+
+*/
+
+
+int[] Insertion_Sort(int[] tab){
+    int temp;
+    for (int i=0; i<tab.Length; i++) //Outer loop = O(n)
+    {
+        for (int j=i; j>0; j--){ //Inner loop = O(n)
+            if (tab[j] < tab[j-1])
+            {
+                temp = tab[j];
+                tab[j] = tab[j-1];
+                tab[j-1] =  temp;
+            }
+            else 
+                break;
+        }
+    }
+    return tab;
+}   
+
+
+int[] in_tab = {5, 9, 3, 1, 2, 8, 4, 7, 6};
+int[] out_tab = Insertion_Sort(in_tab);
+
+Console.WriteLine("[ " + string.Join(", ", out_tab) + " ]");
